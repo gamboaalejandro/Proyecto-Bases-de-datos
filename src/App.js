0ae89122e56919ae932f437f68694aced96360b0
Iagovisk <66756953+Iagovisk@users.noreply.github.com>
2021-12-28 18:58:53 -0400

    Ultimo React

--- src/App.js
+++ src/App.js
@@ -11,11 +11,11 @@ function App() {
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://c.tenor.com/3DRRn-B8_dcAAAAS/feliz-contento.gif"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Da clic para alegrarte
         </a>
       </header>
     </div>
