ded075207a49d785a6703f07ce1306da29dff89e
Iagovisk <66756953+Iagovisk@users.noreply.github.com>
2021-12-28 19:54:55 -0400

    Organizado en carpeta

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <label>Hola</label>
+        <input>Hola</input>
+        <button>Presionar</button>
         <p>
           Bienvenido a la perdicion del semestre, pelaras bola parejo mi pana!
         </p>
