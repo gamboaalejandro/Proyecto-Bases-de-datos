63aa307b39d6ff5eca25e9f9d58be3763f115ec9
Gamboa <agamboacj@gmail.com>
2022-01-06 19:58:20 -0400

    Los illusionistas que buena pelicula joder

--- src/routes/links.js
+++ src/routes/links.js
@@ -8,16 +8,39 @@ router.get('/Producto', (req, res, next) => {
 })
 
 router.post('/Producto', async(req, res, next) => {
+    const varr = req.body;
+    console.log(varr);
+    //var Nro_Producto = producto.Producto;
+    await pool.query("INSERT into producto set ? ", {
+        id_producto: 99,
+        nombre: "Falda",
+        Precio: 9.8,
+        Caracteristicas: 'Roja xl',
+        Materiales: 'PLASTICO',
+        Montaje: 0,
+        Id_categoriaP: 10,
+        Cantidad: 10,
+        'Instrucciones de uso': 'ponersela'
+    });
+    const Query = null;
+    console.log(Query);
+    //var caracteristica = Query[0].Caracteristicas;
+    //var algo = caracteristica.valueOf();
+    //console.log(algo);   
+    res.render('links/Producto', { Query });
+});
+/*
+router.post('/Producto', async(req, res, next) => {
     var producto = req.body;
     var Nro_Producto = producto.Producto;
-    await pool.query("Select * from Producto where id_producto = ? ", Nro_Producto);
+    //await pool.query("INSERT into producto set ?", [producto]);
     const Query = await pool.query("Select * from Producto where id_producto = ? ", Nro_Producto);
     console.log(Query);
     //var caracteristica = Query[0].Caracteristicas;
     //var algo = caracteristica.valueOf();
-    //console.log(algo);
-    res.render('links/Producto', { Query });
-});
+    //console.log(algo); 
+    next();
+});*/
 
 router.get('/', async(req, res, next) => {
     res.send('algo');
