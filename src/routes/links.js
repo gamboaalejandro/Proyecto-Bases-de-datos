11321d76f6a01a67e95ba3ab66507affd7dc18dc
Iagovisk <66756953+Iagovisk@users.noreply.github.com>
2022-01-06 18:09:04 -0400

    Corregido error visual

--- src/routes/links.js
+++ src/routes/links.js
@@ -12,15 +12,16 @@ router.post('/Producto', async(req, res, next) => {
     var Nro_Producto = producto.Producto;
     await pool.query("Select * from Producto where id_producto = ? ", Nro_Producto);
     const Query = await pool.query("Select * from Producto where id_producto = ? ", Nro_Producto);
-    var caracteristica = Query[0].Caracteristicas;
-    var algo = caracteristica.valueOf();
-    console.log(algo);
     console.log(Query);
-    res.render('links/Producto', { Query, algo });
+    //var caracteristica = Query[0].Caracteristicas;
+    //var algo = caracteristica.valueOf();
+    //console.log(algo);
+    res.render('links/Producto', { Query });
 });
 
 router.get('/', async(req, res, next) => {
     res.send('algo');
+
 });
 
 module.exports = router;
