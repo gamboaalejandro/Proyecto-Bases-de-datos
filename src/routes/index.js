d9378008b9b6d19696ebc8dd93b8099f927d8e72
Gamboa <agamboacj@gmail.com>
2022-01-12 01:30:44 -0400

    Cambios en el buscar

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.render("links/subcategorias");
+    res.render("links/indexFix");
 });
 
 module.exports = router;
