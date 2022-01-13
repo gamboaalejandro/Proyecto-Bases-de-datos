b3b15826fdd887a0a7885f55e27c7ab4e8ee53ca
Gamboa <agamboacj@gmail.com>
2022-01-13 12:40:08 -0400

    Al fin arrelge el peo de que no envia post

--- src/routes/links.js
+++ src/routes/links.js
@@ -4,11 +4,36 @@ const router = express.Router();
 const pool = require('../database');
 
 
+router.get('/modificar/:id_producto', async(req, res, next) => {
+    var producto = req.params.id_producto;
+    console.log('putaputa')
+    const Query = await pool.query("Select * from Producto where id_producto = ? ", [producto]);
+    console.log(Query);
+    res.render('links/ProductoModificar', { Query: Query[0] })
+});
+router.post('/modificar/:id_producto', async(req, res, next) => {
 
-router.get('/Producto/nuevo', (req, res, next) => {
-    //res.send('algo');
-    console.log("-----get", );
-    res.render('links/Producto');
+    const varr = req.body;
+    console.log(varr.id_p);
+    /* if ((varr.id_producto !== "") && (varr.Nombre !== "") && (varr.Precio !== "") && (varr.CategoriaID !== "") && (varr.cantidad !== "") && (varr.materiales !== "") && (varr.Caracteristicas !== "") && (varr.Instrucciones !== "")) {
+         console.log("-----post");
+         //var Nro_Producto = producto.Producto;
+         await pool.query("INSERT into producto set ? ", {
+             id_producto: varr.id_producto,
+             nombre: varr.Nombre,
+             Precio: varr.Precio,
+             Id_categoriaP: varr.CategoriaID,
+             Caracteristicas: varr.Caracteristicas,
+             Materiales: varr.materiales,
+             Montaje: varr.Montaje,
+             Cantidad: varr.cantidad,
+             Instrucciones: varr.Instrucciones
+         });
+     } else {
+         // usar libreria pop up 
+         res.send("ta malo maldita");
+     }*/
+    res.send("ta malo maldita");
 })
 
 router.get('/Producto/editar', (req, res, next) => {
@@ -20,7 +45,7 @@ router.get('/Producto/editar', (req, res, next) => {
 router.get('/ProductoGuardar', async(req, res, next) => {
     res.render('links/ProductoGuardar');
 });
-
+// FUNCIOA PARA ANADIR UN PRODUCTO
 router.post('/ProductoGuardar', async(req, res, next) => {
     const varr = req.body;
     console.log(varr.id_producto);
@@ -46,7 +71,6 @@ router.post('/ProductoGuardar', async(req, res, next) => {
     } else {
         // usar libreria pop up 
         res.send("ta malo maldita");
-
     }
     res.render('links/ProductoGuardar');
 
