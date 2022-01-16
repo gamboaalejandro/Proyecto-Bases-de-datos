19b48c7f6028097b610b9b656225872b89af420a
Iagovisk <66756953+Iagovisk@users.noreply.github.com>
2022-01-16 14:17:20 -0400

    Login agregado

--- src/routes/links.js
+++ src/routes/links.js
@@ -249,5 +249,10 @@ router.get('/tiendas/indexFixCiudadMexico', async(req, res, next) => {
     res.render('links/tiendas/indexFixCiudadMexico');
 })
 
+router.get('/login', async(req, res, next) => {
+    res.render('links/login');
+})
+
+
 
 module.exports = router;
